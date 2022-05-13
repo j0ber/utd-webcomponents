13d0a9040e887f1b6fd6c57deb58540a38a94bc7
j0ber <chriscaux@hotmail.com>
2022-05-13 11:07:51 -0400

    Ajout exemples

--- src/siteDemo/pages/Notifications.svelte
+++ src/siteDemo/pages/Notifications.svelte
@@ -1,92 +1,155 @@
 <script>
-  import { onMount } from 'svelte';
-  import CodeSource from '../components/CodeSource.svelte'; 
+    import { onMount } from 'svelte';
+    import CodeSource from '../components/CodeSource.svelte'; 
+    let mounted = false;
 
-  let resultat1 = ''
-  let resultat2 = ''
-  let resultat3 = ''
-  let resultat4 = ''
-  let resultat5 = ''
-  let resultat6 = ''
+    onMount(() => {
+        notification1();
+        notification2();
+        notification3();
+        notification4();
+        notification5();
+        notification6();
+        mounted = true;
+    });
 
-  onMount(() => {
+    function notification1() {
+        document.getElementById('test1').addEventListener('click', () => {
+            const params = {
+                titre: "Bravo champion!",
+                message: "Tu as réussi à enregistrer le formulaire."
+                };
 
-    document.getElementById('btnTest1').addEventListener('click', () => {
-        const params = {
-             titre: "Bravo champion!",
-             message: "Tu as réussi à enregistrer le formulaire."
-            }
+            utd.notification.emettre(params);
+        });
+    }
 
-        utd.notification.emettre(params);
-    })
+    function notification2() {
+        document.getElementById('test2').addEventListener('click', () => {
+            const params = {
+                message: "Tu as réussi à enregistrer comme un grand."
+                };
 
-    document.getElementById('btnTest2').addEventListener('click', () => {
-        const params = {
-             message: "Tu as réussi à enregistrer comme un grand."
-            }
+            utd.notification.emettre(params);
+        });
+    }
 
-        utd.notification.emettre(params);
-    })
+    function notification3() {
+        document.getElementById('test3').addEventListener('click', () => {
+            const params = {
+                type: "negatif",
+                titre: "Erreur",
+                message: "Une erreur s'est produite."
+                };
 
-    document.getElementById('btnTest3').addEventListener('click', () => {
-        const params = {
-             type: "negatif",
-             titre: "Erreur",
-             message: "Une erreur s'est produite."
-            }
+            utd.notification.emettre(params);
+        });
+    }
 
-        utd.notification.emettre(params);
-    })
+    function notification4() {
+        document.getElementById('test4').addEventListener('click', () => {
+            const params = {
+                type: "negatif",
+                message: "Une erreur s'est produite."
+                };
 
-    document.getElementById('btnTest4').addEventListener('click', () => {
-        const params = {
-             type: "negatif",
-             message: "Une erreur s'est produite."
-            }
+            utd.notification.emettre(params);
+        });
+    }
 
-        utd.notification.emettre(params);
-    })
+    function notification5() {
+        document.getElementById('test5').addEventListener('click', () => {
+            const params = {
+                type: "neutre",
+                titre: "À noter",
+                message: "Chuck Norris est incroyable. Il a compté jusqu'à l'infini. 2 fois!"
+                };
 
-    document.getElementById('btnTest5').addEventListener('click', () => {
-        const params = {
-             type: "neutre",
-             titre: "À noter",
-             message: "Chuck Norris est incroyable. Il a compté jusqu'à l'infini. 2 fois!"
-            }
+            utd.notification.emettre(params);
+        });
+    }
 
-        utd.notification.emettre(params);
-    })
+    function notification6() {
+        document.getElementById('test6').addEventListener('click', () => {
+            const params = {
+                type: "neutre",
+                message: "Chuck Norris est incroyable. Il a compté jusqu'à l'infini. 2 fois!"
+                };
 
-    document.getElementById('btnTest6').addEventListener('click', () => {
-        const params = {
-             type: "neutre",
-             message: "Chuck Norris est incroyable. Il a compté jusqu'à l'infini. 2 fois!"
-            }
-
-        utd.notification.emettre(params);
-    })
-})
-
+            utd.notification.emettre(params);
+        });
+    }
 </script>
 
 <h1>Notifications</h1>
 <h2>Notification de type "positif" avec titre</h2>
-<button type="button" id="btnTest1" class="utd-btn secondaire mb-32">Test 1</button>
+<button type="button" id="test1" class="utd-btn secondaire mb-16">Test 1</button>
+{#if mounted}
+    <utd-accordeon titre="Code source">
+        <span slot="contenu">
+            <CodeSource codeSource="{notification1.toString()}" language="language-javascript">
+            </CodeSource>   
+        </span>
+    </utd-accordeon>
+{/if}   
 
 <h2>Notification de type "positif" sans titre</h2>
-<button type="button" id="btnTest2" class="utd-btn secondaire mb-32">Test 2</button>
+<button type="button" id="test2" class="utd-btn secondaire mb-16">Test 2</button>
+{#if mounted}
+    <utd-accordeon titre="Code source">
+        <span slot="contenu">
+            <CodeSource codeSource="{notification2.toString()}" language="language-javascript">
+            </CodeSource>   
+        </span>
+    </utd-accordeon>
+{/if}   
 
 <h2>Notification de type "negatif" avec titre</h2>
-<button type="button" id="btnTest3" class="utd-btn secondaire mb-32">Test 3</button>
+<button type="button" id="test3" class="utd-btn secondaire mb-16">Test 3</button>
+{#if mounted}
+    <utd-accordeon titre="Code source">
+        <span slot="contenu">
+            <CodeSource codeSource="{notification3.toString()}" language="language-javascript">
+            </CodeSource>   
+        </span>
+    </utd-accordeon>
+{/if}   
+
 
 <h2>Notification de type "negatif" sans titre</h2>
-<button type="button" id="btnTest4" class="utd-btn secondaire mb-32">Test 4</button>
+<button type="button" id="test4" class="utd-btn secondaire mb-16">Test 4</button>
+{#if mounted}
+    <utd-accordeon titre="Code source">
+        <span slot="contenu">
+            <CodeSource codeSource="{notification4.toString()}" language="language-javascript">
+            </CodeSource>   
+        </span>
+    </utd-accordeon>
+{/if}   
+
 
 <h2>Notification de type "neutre" avec titre</h2>
-<button type="button" id="btnTest5" class="utd-btn secondaire mb-32">Test 5</button>
+<button type="button" id="test5" class="utd-btn secondaire mb-16">Test 5</button>
+{#if mounted}
+    <utd-accordeon titre="Code source">
+        <span slot="contenu">
+            <CodeSource codeSource="{notification5.toString()}" language="language-javascript">
+            </CodeSource>   
+        </span>
+    </utd-accordeon>
+{/if}   
+
 
 <h2>Notification de type "neutre" sans titre</h2>
-<button type="button" id="btnTest6" class="utd-btn secondaire mb-32">Test 6</button>
+<button type="button" id="test6" class="utd-btn secondaire mb-16">Test 6</button>
+{#if mounted}
+    <utd-accordeon titre="Code source">
+        <span slot="contenu">
+            <CodeSource codeSource="{notification6.toString()}" language="language-javascript">
+            </CodeSource>   
+        </span>
+    </utd-accordeon>
+{/if}   
 
 <style>
 </style>
