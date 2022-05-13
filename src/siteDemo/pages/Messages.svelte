390139e5181f64c6f5c543316a2869872e838407
j0ber <chriscaux@hotmail.com>
2022-05-13 16:01:15 -0400

    WIP tables

--- src/siteDemo/pages/Messages.svelte
+++ src/siteDemo/pages/Messages.svelte
@@ -113,7 +113,54 @@
 </script>
 
 <h1>Messages</h1>
-<h2>Message de type avertissement avec 2 boutons</h2>
+
+<h2>Paramètres</h2>
+<table class="utd-table bordures-lignes">
+    <thead>
+        <tr>
+            <th style="width: 100px;">Nom</th>
+            <th style="width: 80px;">Type</th>
+            <th>Description</th> 
+        </tr>
+    </thead> 
+    <tbody>
+        <tr>
+            <th><span>type</span></th>
+            <td>String</td> 
+            <td>Type de message. Valeurs possibles : "" (Défaut, aucun type), "avertissement", "succes", "erreur".</td>
+
+        </tr>
+        <tr>
+            <th><span>titre</span></th>
+            <td>String</td> 
+            <td>Titre du message.</td>
+        </tr>
+        <tr>
+            <th>corps</th>
+            <td>String</td>  
+            <td>Corps du message.</td>
+        </tr>
+        <tr>
+            <th>texteBoutonPrimaire</th>
+            <td>String</td>  
+            <td>Texte du bouton primaire. Si non spécifié, le bouton primaire n'est pas affiché.</td>
+        </tr>
+        <tr>
+            <th>texteBoutonSecondaire</th>
+            <td>String</td>  
+            <td>Texte du bouton secondaire. Si non spécifié, le bouton secondaire n'est pas affiché.</td>
+        </tr>
+        <tr>
+            <th>estBoutonsTexteLong</th>
+            <td>Boolean</td>  
+            <td>Indique si le texte des boutons est long. Si c'est le cas, les boutons sont affichés verticalement en pleine largeur si la largeur d'écran est inférieure ou égale à 525px. (Au lieu de 425px normalement)</td>
+        </tr>
+
+    </tbody>
+</table>
+
+<h2>Exemples</h2>
+<h3>Message de type avertissement avec 2 boutons</h3>
 <button type="button" id="btnTest1" class="utd-btn secondaire mb-16">Test 1</button>
 <div id="resultat1"></div>
 {#if mounted}
@@ -126,7 +173,7 @@
 {/if}   
 
 
-<h2>Message de type information avec 1 bouton</h2>
+<h3>Message de type information avec 1 bouton</h3>
 <button type="button" id="btnTest2" class="utd-btn secondaire mb-16">Test 2</button>
 <div id="resultat2"></div>
 {#if mounted}
@@ -139,7 +186,7 @@
 {/if}   
 
 
-<h2>Message de type succès avec 1 bouton</h2>
+<h3>Message de type succès avec 1 bouton</h3>
 <button type="button" id="btnTest3" class="utd-btn secondaire mb-16">Test 3</button>
 <div id="resultat3"></div>
 {#if mounted}
@@ -152,7 +199,7 @@
 {/if}   
 
 
-<h2>Message de type erreur avec 1 bouton</h2>
+<h3>Message de type erreur avec 1 bouton</h3>
 <button type="button" id="btnTest4" class="utd-btn secondaire mb-16">Test 4</button>
 <div id="resultat4"></div>
 {#if mounted}
@@ -165,7 +212,7 @@
 {/if}   
 
 
-<h2>Message sans type avec 2 boutons</h2>
+<h3>Message sans type avec 2 boutons</h3>
 <button type="button" id="btnTest5" class="utd-btn secondaire mb-16">Test 5</button>
 <div id="resultat5"></div>
 {#if mounted}
@@ -178,7 +225,7 @@
 {/if}   
 
 
-<h2>Message sans type avec 2 boutons et texte long</h2>
+<h3>Message sans type avec 2 boutons et texte long</h3>
 <button type="button" id="btnTest6" class="utd-btn secondaire mb-16">Test 6</button>
 <div id="resultat6"></div>
 {#if mounted}
